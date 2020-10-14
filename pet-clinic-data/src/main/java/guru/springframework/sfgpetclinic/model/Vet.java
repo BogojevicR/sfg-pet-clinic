662f012acbb2a25c2ecd7e5ab5a3df5658553cf9
BogojevicR <ralebogojevic@gmail.com>
2020-10-14 17:31:02 +0200

    adding project lombok

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,22 +1,22 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table( name = "vets")
 public class Vet extends Person {
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
-    private Set<Specialty> specialties = new HashSet<>();
+    private Set<Specialty> specialities = new HashSet<>();
 
-    public Set<Specialty> getSpecialities() {
-        return specialties;
-    }
-
-    public void setSpecialities(Set<Specialty> specialties) {
-        this.specialties = specialties;
-    }
 }
